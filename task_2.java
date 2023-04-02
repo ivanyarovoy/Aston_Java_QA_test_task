a3aadccf55ce5f931de52360e126ece399a8df0d
ivanyarovoy <93982780+ivanyarovoy@users.noreply.github.com>
2023-04-02 23:02:04 +0300

    Update task_2.java

--- task_2.java
+++ task_2.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 public class Main
 {
-	public static void main(String[] args) {
-	    String vyacheslav = "Вячеслав";
-	    Scanner myInput = new Scanner( System.in);
-      String str = myInput.nextLine();
-      if (str.equals(vyacheslav)) {
-          System.out.println("Привет, Вячеслав");
+  public static void main (String[]args)
+  {
+    String vyacheslav = "Вячеслав";
+    Scanner myInput = new Scanner (System.in);
+    String str = myInput.nextLine ();
+    if (str.equals (vyacheslav))
+      {
+	System.out.println ("Привет, Вячеслав");
       }
-      else{
-          System.out.println("Нет такого имени");
+    else
+      {
+	System.out.println ("Нет такого имени");
       }
-	}
+  }
 }
