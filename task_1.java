051efe7088dece016766da0dae3509b2be597790
ivanyarovoy <93982780+ivanyarovoy@users.noreply.github.com>
2023-04-02 23:04:09 +0300

    Update task_1.java

--- task_1.java
+++ task_1.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 public class Main
 {
-	public static void main(String[] args) {
-	    Scanner myInput = new Scanner( System.in );
-        int a = myInput.nextInt();
-        if (a > 7) {
-            System.out.println("Привет");
-        }
-	}
+  public static void main (String[]args)
+  {
+    Scanner myInput = new Scanner (System.in);
+    int a = myInput.nextInt ();
+    if (a > 7)
+      {
+	System.out.println ("Привет");
+      }
+  }
 }
