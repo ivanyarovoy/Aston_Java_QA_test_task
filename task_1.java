f50c4b1105ab4db1f3919c62261618b76fe23ade
ivanyarovoy <93982780+ivanyarovoy@users.noreply.github.com>
2023-04-02 22:50:42 +0300

    Create task_1.java

---
+++ task_1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner myInput = new Scanner( System.in );
+        int a = myInput.nextInt();
+        if (a > 7) {
+            System.out.println("Привет");
+        }
+	}
+}
